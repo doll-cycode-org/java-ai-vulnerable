c17cde504ddad9c5f0befd279c7d7aef4420b6ea
adoll-cycode <austin.doll@cycode.com>
2026-04-08 13:54:30 -0400

    teasr

---
+++ unsanitized_input copy 3.py
@@ -0,0 +1,38 @@
+import sqlite3
+import os
+import subprocess
+
+# Vulnerable to SQL injection
+def get_user(username):
+    conn = sqlite3.connect("users.db")
+    cursor = conn.cursor()
+    query = "SELECT * FROM users WHERE username = '" + username + "'"
+    cursor.execute(query)
+    return cursor.fetchall()
+
+# Vulnerable to command injection
+def ping_host(host):
+    output = os.popen("ping -c 1 " + host).read()
+    return output
+
+# Vulnerable to path traversal
+def read_file(filename):
+    with open("/var/data/" + filename, "r") as f:
+        return f.read()
+
+# Vulnerable to eval injection
+def calculate(expression):
+    return eval(expression)
+
+if __name__ == "__main__":
+    username = input("Enter username: ")
+    print(get_user(username))
+
+    host = input("Enter host to ping: ")
+    print(ping_host(host))
+
+    filename = input("Enter filename to read: ")
+    print(read_file(filename))
+
+    expr = input("Enter expression to evaluate: ")
+    print(calculate(expr))
